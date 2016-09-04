634617c3c09cf0de582d1f2bae58656d3ae9e7e2
Oren <orengolan@gmail.com>
2016-09-04 13:18:28 -0700

    init

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM alpine:3.3
+WORKDIR /src
+ADD backend /src/backend
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", hello)
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello")
+
+}
